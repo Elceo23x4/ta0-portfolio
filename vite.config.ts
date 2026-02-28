709be003aa18230a6b667137db4ac42fd85ebac5
Elceo <142925108+Elceo23x4@users.noreply.github.com>
2026-02-28 15:29:02 +0000

    Update for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ta0-portfolio/',
+  base: '/',
   build: {
     outDir: 'dist', 
     },
